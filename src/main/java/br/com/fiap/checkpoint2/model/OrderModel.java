2147e822ee3200174803eecf3a4db3421666f968
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-03 08:52:05 -0300

    passo-15-concluido

--- src/main/java/br/com/fiap/checkpoint2/model/OrderModel.java
+++ src/main/java/br/com/fiap/checkpoint2/model/OrderModel.java
@@ -1,11 +1,42 @@
 package br.com.fiap.checkpoint2.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class OrderModel {
+    @Id
+    @Getter
     private long id;
+    @Setter
+    @Getter
     private String clientName;
+    @Setter
+    @Getter
     private LocalDate orderDate;
+    @Setter
+    @Getter
     private BigDecimal totalValue;
+
+
+    public void prePersist(){
+        if(orderDate==null){
+            LocalDate orderDate = LocalDate.now();
+
+        }
+
+    }
+
 }
+
+
