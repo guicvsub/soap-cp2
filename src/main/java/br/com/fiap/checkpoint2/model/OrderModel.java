faf2f6b81a444fd7fe950a2ac82fe31c3f866c29
guicvsub <rm552321@fiap.com.br>
2025-10-03 22:42:52 -0300

    melhorando-comentario

--- src/main/java/br/com/fiap/checkpoint2/model/OrderModel.java
+++ src/main/java/br/com/fiap/checkpoint2/model/OrderModel.java
@@ -30,7 +30,7 @@ public class OrderModel {
     private LocalDate orderDate;
     //@Positive(message = "o numero so pode ser positivo") //esta linha esta representada na linha 35 equivalente a passo 21
     //@DecimalMin(value = "0.0") // prof eu acho que nao precisa dessa linha (mais ta no roteiro) equivalente a passo 20
-    // porque a notacao @Positive garante que nao seja zero,  enquanto a linha 31 garante o zero
+    // porque a notacao @Positive garante que nao seja zero (porque zero nao e positivo),  enquanto a linha 32 garante o zero
     // nesse caso a melhor opcao e @PossitiveOrZero ai ta de boa
     @PositiveOrZero(message = "O valor total não pode ser negativo")
     private BigDecimal totalValue;
